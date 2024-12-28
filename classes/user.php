feb6bf51f92cccd256ae342d4e7a031bc8861462
salma hamdi <saalmahm51@gmail.com>
2024-12-28 11:20:57 +0100

    completer les classes : user , client

--- classes/user.php
+++ classes/user.php
@@ -9,7 +9,6 @@ class User
     protected $email;
     protected $role;
 
-
     public function __construct(PDO $db, $id, $first_name, $last_name, $email, $role)
     {
         $this->db = $db;
@@ -30,12 +29,21 @@ class User
         return "$this->firstName $this->lastName";
     }
 
-    public function getRole(){
+    public function getRole()
+    {
         return $this->role;
     }
 
     public function editInfo($newFirstName, $newLastName)
     {
+        $this->firstName = $newFirstName;
+        $this->lastName = $newLastName;
 
+        $stmt = $this->db->prepare("UPDATE users SET first_name = :first_name, last_name = :last_name WHERE id = :id");
+        $stmt->execute([
+            ':first_name' => $newFirstName,
+            ':last_name' => $newLastName,
+            ':id' => $this->id
+        ]);
     }
 }
