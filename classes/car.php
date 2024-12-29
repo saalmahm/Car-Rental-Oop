c342e71533a328dd32122fa381c9fad539ea38e6
MohamedHamouch <med10hamouch@gmail.com>
2024-12-29 03:53:30 +0100

    define admin methods

--- classes/car.php
+++ classes/car.php
@@ -2,26 +2,28 @@
 
 class Car
 {
+    private $id;
     private $immatriculation;
     private $marque;
-    private $model;
+    private $modele;
     private $disponibilite;
 
-    public function __construct($immatriculation, $marque, $model, $disponibilite)
+    public function __construct($immatriculation, $marque, $modele, $disponibilite)
     {
         $this->immatriculation = $immatriculation;
         $this->marque = $marque;
-        $this->model = $model;
+        $this->modele = $modele;
         $this->disponibilite = $disponibilite;
     }
 
     public function getAttributes()
     {
         return [
+            "id"=> $this->id,
             'immatriculation' => $this->immatriculation,
             'marque' => $this->marque,
-            'model' => $this->model,
-            'disponiblite' => $this->disponibilite
+            'modele' => $this->modele,
+            'disponibilite' => $this->disponibilite
         ];
     }
 }
