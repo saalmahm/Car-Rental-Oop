77eaac4ac7c913c857d71c124073c1219152d7a6
MohamedHamouch <med10hamouch@gmail.com>
2024-12-29 03:18:31 +0100

    update profile form handling with class user method

---
+++ pages/processes/update_profile.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require_once '../../classes/database.php';
+require_once '../../classes/authentification.php';
+require_once '../../classes/client.php';
+
+$auth = new Authentication($db->getConnection());
+
+if (!$auth->isLoggedIn()) {
+    header('Location: ../login.php');
+    exit();
+} else {
+
+    $user = new Client(
+        $db->getConnection(),
+        $_SESSION['userId'],
+        $_SESSION['firstName'],
+        $_SESSION['lastName'],
+        $_SESSION['email'],
+        $_SESSION['role']
+    );
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['firstName']) && isset($_POST['lastName'])) {
+        $firstName = trim($_POST['firstName']);
+        $lastName = trim($_POST['lastName']);
+
+        $user->editInfo($firstName, $lastName);
+        header('Location: ../profile.php');
+        exit();
+    }
+} else {
+
+    header('Location: ../profile.php');
+    exit();
+}
